cfa7b674ce8e7691baf39072994eaaac29df8297
Joshua Eckroth <eckroth@cse.ohio-state.edu>
2016-01-09 10:39:58 -0500

    Working domjudge installation

--- reload-db.sh
+++ reload-db.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-MYSQLROOT=`grep -oE 'mysql_root_password: .*' ansible/vars/domserver-passwords.yml | sed -e 's/mysql_root_password: //'`
+MYSQLROOT=`grep -oE 'mysql_root_password: .*' ansible/vars/common-passwords.yml | sed -e 's/mysql_root_password: //'`
 SCPOPTS=`vagrant ssh-config domjudgegrader | grep -v '^Host ' | grep -v '^$' | awk -v ORS=' ' '{print "-o " $1 "=" $2}'`
 IP=`echo ${SCPOPTS} | grep -oE '((1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])\.){3}(1?[0-9][0-9]?|2[0-4][0-9]|25[0-5])'`
 scp ${SCPOPTS} domjudge.sql vagrant@${IP}:/home/vagrant/domjudge.sql
